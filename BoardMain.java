d5240b46111201d814f9da502a72dbd762771a51
jvisionInformation <wsj8648@gmail.com>
2018-01-25 11:10:03 +0900

    Update BoardMain.java
    업데이트 되었습니다

--- BoardMain.java
+++ BoardMain.java
@@ -5,6 +5,11 @@ public class BoardMain {
 		dock.init(); // this -> dock
 		for(int i=0; i<dock.card.length; i++) {
 			System.out.println(dock.card[i].kind+","+dock.card[i].num);
+
+			//0   12 s
+			//13  25 c
+			//26  38 h
+			//39  51 d
 		}
 	}
 }
