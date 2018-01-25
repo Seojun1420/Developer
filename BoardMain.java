dd5e1feb183ff5a55d2ca68df2d11f386b2eb659
jvisionInformation <wsj8648@gmail.com>
2018-01-25 12:30:43 +0900

    Update BoardMain.java
    업데이트 추가

--- BoardMain.java
+++ BoardMain.java
@@ -1,19 +1,20 @@
 public class BoardMain {
 	public static void main(String[] args) {
-		Dock dock = new Dock(); 
-		System.out.println(dock.card); // null
-		dock.init(); // this -> dock
-		for(int i=0; i<dock.card.length; i++) {
-			System.out.println(dock.card[i].kind+","+dock.card[i].num);
+		Deck deck = new Deck(); 
+		System.out.println(deck.card); // null
+		deck.init(); // this -> dock
+		for(int i=0; i<deck.card.length; i++) {
+			System.out.println(deck.card[i].kind+","+deck.card[i].num);
 		}
 		System.out.println("-------------------------------");
 		// dock 섞기
-		dock.shuffle();
+		deck.shuffle();
 		// dock섞기 확인 
-		for(int i=0; i<dock.card.length; i++) {
-			System.out.println(dock.card[i].kind+","+dock.card[i].num);
+		for(int i=0; i<deck.card.length; i++) {
+			System.out.println(deck.card[i].kind+","+deck.card[i].num);
 
 		
 	}
 }
 }
+
