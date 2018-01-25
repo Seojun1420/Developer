bb3dbd7237fbb97660296c66e1865eb250d07044
jvisionInformation <wsj8648@gmail.com>
2018-01-25 10:22:45 +0900

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,19 +1,4 @@
-
 public class Card {
-	private String kind;
-	private int num;
-	public String getKind() {
-		return kind;
-	}
-	public void setKind(String kind) {
-		this.kind = kind;
-	}
-	public int getNum() {
-		return num;
-	}
-	public void setNum(int num) {
-		this.num = num;
-	}
-	
-
+	public String kind;
+	public int num;
 }
