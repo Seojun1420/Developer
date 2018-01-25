ad1abc353056aabd766975f70a3f90d6c08bd854
jvisionInformation <wsj8648@gmail.com>
2018-01-25 10:26:47 +0900

    Create BoardMain.java

---
+++ BoardMain.java
@@ -0,0 +1,10 @@
+public class BoardMain {
+	public static void main(String[] args) {
+		Dock dock = new Dock(); 
+		System.out.println(dock.card); // null
+		dock.init(); // this -> dock
+		for(int i=0; i<dock.card.length; i++) {
+			System.out.println(dock.card[i].kind+","+dock.card[i].num);
+		}
+	}
+}
