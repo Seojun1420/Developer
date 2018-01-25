ea686e2c990fae57549938e437925878d8dc6c81
jvisionInformation <wsj8648@gmail.com>
2018-01-25 09:18:28 +0900

    Card.java

---
+++ Card.java
@@ -0,0 +1,19 @@
+
+public class Card {
+	private String kind;
+	private int num;
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+
+}
